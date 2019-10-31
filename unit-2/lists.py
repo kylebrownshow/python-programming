610969037a6c19d42f85201f50c60e9ec36a5339
Brown <kbrown2@webershandwick.com>
2019-10-30 20:18:57 -0400

    nested loops

--- unit-2/lists.py
+++ unit-2/lists.py
@@ -27,7 +27,7 @@ print (cohort_4)
 cohort_4.remove('julian')
 
 #create a new list with only the floats
-'''
+
 values = [2.3, 45, 11, -5, 3.5, 7.9, 11.7, 40, 85.6, 77.1]
 float_list = []
 
@@ -37,7 +37,7 @@ for num in values:
 
 print (float_list)
 
-''' also correct answer, because if the number isn't odd or even, it must be a float
+also correct answer, because if the number isn't odd or even, it must be a float
 
 for num in values:
     if num % 2 != 1 and num % 2 !=0:
@@ -45,4 +45,62 @@ for num in values:
 
 print (float_list)
 
+
+#find average of each list and add to a new list
+
+weights = [[50, 25, 75], [95.7, 38.3, 55.2], [88, 45, 16]]
+
+new_list = []
+
+for item in weights:
+    list_total = 0
+    for value in item:
+        list_total += value
+    
+    new_list.append(list_total/len(item))
+    
+print(new_list)
+
+
+#using the same theory of loops within loops, output 
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+
+for row in range(1, 11):
+    for col in range (1, row + 1):
+        print('*', end=' ')
+    print('')
+
+
+for i in range(11):
+    print('*' * (i+1))
+
+
+#using indexes to access list items
+#use index if we need to edit items in list
+
+#set all the negative readings to 0
 '''
+readings = [3.5, -7.7, -9.8, 13.6, 22.5, 19.7, -8.6]
+
+for idx in range(len(readings)):
+    if readings[idx] < 0:
+        readings[idx] = 0
+print(readings)
+
+#find the position of an item in a list
+
+current_age = 25
+ages = [15, 17, 27, 35, 12, 25, 55, 40, 31, 20]
+
+for idx in range(len(ages)):
+    if ages[idx] == current_age:
+        print('25 is found at position ', idx)
