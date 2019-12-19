200bc2d44c21306c734c43f872fb0de6a27fe0c2
kylebrownshow <57117648+kylebrownshow@users.noreply.github.com>
2019-12-19 04:05:02 -0500

    Add files via upload

--- unit-4/order_manager.py
+++ unit-4/order_manager.py
@@ -7,28 +7,58 @@
 
 import json
 
-order_file = 'orders.json'
-
-class Orders:
-    def __init__ (self, id, customer_name, customer_email, orders): #no need to include the empty list, because it's empty and that's obvious
-        self.customer_name = customer_name
-        self.customer_email = customer_email
-        self.orders = orders
-        self.customer_orders = [] 
-
-    def load_orders(self):
-        #load the data from the tracks.txt file  
-        with open(order_file, 'r') as order_file: #track_file_name on line 60
-            data = json.load(order_file)
+with open('orders.json', 'r') as order_file:
+    customer_orders = json.load(order_file)
         #set the tracks variable to the data loaded in
-        self.customer_orders = data
 
-    def count_customers(self):
-        count = 0
-        for customer in customer_name:
-            count += 1
-        print (count)
+print(type(customer_orders))
 
-order_list = Orders('new orders')
-order_list.load_orders()
-print(order_list.Orders)
+#print(customer_orders)
+#1. How many customers are there in all? 
+
+def counter(orders):
+    count = 0
+    for item in orders:
+        count += 1
+    return count
+
+print(f'there are {counter(customer_orders)} customers')
+
+
+#2. Print the name and email address of the customer with id 13 
+
+for cus in customer_orders:
+    if cus['id'] == 13:
+        print (cus['customer_name'], cus['customer_email'])
+
+#3. Print all the items ordered by the customer with id 20. 
+
+for cus in customer_orders:
+    items = []
+    if cus['id'] == 20:
+        for order in cus['orders']:
+            items.append(order['item'])
+        print (items) 
+
+#4. How may customers bought wine? 
+#for cus in customer_orders:
+#    winecount = []
+#    for item in cus['orders']:
+#        if item['items'] == "wine"
+
+#5. What is the total cost of the items ordered by Matti? 
+for cus in customer_orders:
+    count = 0
+    if cus['customer_name'] == "Matti":
+        for price in cus['orders']:
+            count += float(order['price'].replace('$',''))
+        print(count)
+
+#6. Which customer spent the most money on orders?
+
+for cus in customer_orders:
+    count = 0
+    if cus['customer_name'] == "Matti":
+        for price in cus['orders']:
+            count += float(order['price'].replace('$',''))
+        print(count)
